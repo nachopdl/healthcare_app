d33e04f7879ad393f3ae9dba56c478fb5ee1fdd4
nachopdl <124316313+nachopdl@users.noreply.github.com>
2024-06-15 18:30:15 -0500

    Update signup.js

--- health_modular_app/frontend/js/signup.js
+++ health_modular_app/frontend/js/signup.js
@@ -1,20 +1,38 @@
 document.getElementById('signup-form').addEventListener('submit', function (event) {
     event.preventDefault();
+    
+    const firstName = document.getElementById('firstname').value;
+    const lastName = document.getElementById('lastname').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+    const dob = document.getElementById('dob').value;
+    const gender = document.getElementById('gender').value;
+    const height = document.getElementById('height').value;
+    const weight = document.getElementById('weight').value;
+    const bloodType = document.getElementById('bloodtype').value;
 
     fetch('http://localhost:5000/api/signup', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ email, password })
+        body: JSON.stringify({ 
+            firstName,
+            lastName,
+            email, 
+            password, 
+            dob, 
+            gender, 
+            height: parseInt(height), 
+            weight: parseInt(weight), 
+            blood_type: bloodType 
+        })
     })
     .then(response => response.json())
     .then(data => {
         if (data.success) {
             alert('Registration successful!');
-            window.location.href = 'dashboard'; // Redirige al login después de registrarse
+            window.location.href = 'dashboard'; // Redirige al dashboard después de registrarse
         } else {
             alert('Registration failed: ' + data.message);
         }
