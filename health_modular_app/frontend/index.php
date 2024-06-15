97e07da0ca63e1db475f0c5ab8ef4c5559ed77d4
nachopdl <124316313+nachopdl@users.noreply.github.com>
2024-06-14 19:41:29 -0500

    Add files via upload

---
+++ health_modular_app/frontend/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Healthcare - Home</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="images/logo.webp" alt="Logo">
+                <span>MediConnect</span>
+            </div>
+            <div class="auth-buttons">
+                <a href="login.php" class="login-button">Iniciar Sesión</a>
+                <a href="signup.php" class="signup-button">Registrarte</a>
+            </div>
+            <ul class="nav-links">
+                <li><a href="#">Nosotros</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="hero">
+        <div class="hero-content">
+            <h1>CUIDA TU SALUD VIRTUALMENTE SOLO PARA TI</h1>
+            <p>Con nuestro sistema web completo puedes tener consultas y diagnosticos sobre salud fisica y mental</p>
+            <a href="#" class="cta-button">Consulta hoy</a>
+        </div>
+        <div class="hero-image">
+            <img src="images/index.webp" alt="Healthcare Image">
+        </div>
+    </section>
+    <section class="services">
+        <h2>Nuestros servicios</h2>
+        <p>Ofrecemos servicios de alto valor como nuestros usuarios lo son</p>
+        <div class="service-items">
+            <div class="service-item">
+                <h3>Diagnostico</h3>
+                <p>Diagnostico de enfermedades fisicas y psicologicas.</p>
+            </div>
+            <div class="service-item">
+                <h3>Nutricion</h3>
+                <p>Toma de medidas y alimentacion para darte una mejor nutrición.</p>
+            </div>
+            <div class="service-item">
+                <h3>Psicologia</h3>
+                <p>deteccion de emociones y ayuda en mejorar tu salud mental.</p>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
